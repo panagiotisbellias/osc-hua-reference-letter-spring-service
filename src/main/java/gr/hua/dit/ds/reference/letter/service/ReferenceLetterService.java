e7dc53414251d5dbb129416e081e8dee56627ca5
Panagiotis Bellias <belliaspanagiotis@gmail.com>
2022-01-05 11:57:52 +0200

    renamed main java class

--- src/main/java/gr/hua/dit/ds/reference/letter/service/ReferenceLetterService.java
+++ src/main/java/gr/hua/dit/ds/reference/letter/service/ReferenceLetterService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class ReferenceLetterService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+		SpringApplication.run(ReferenceLetterService.class, args);
 	}
 
 }
--- src/test/java/gr/hua/dit/ds/reference/letter/service/ReferenceLetterServiceTests.java
+++ src/test/java/gr/hua/dit/ds/reference/letter/service/ReferenceLetterServiceTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootApplicationTests {
+class ReferenceLetterServiceTests {
 
 	@Test
 	void contextLoads() {

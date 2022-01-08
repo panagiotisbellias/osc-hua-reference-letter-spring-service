e6f5e93f0b66bea59adc904dbff29b041c720325
Panagiotis Bellias <belliaspanagiotis@gmail.com>
2022-01-08 12:53:19 +0200

    security support is being added

--- README.md
+++ README.md
@@ -15,6 +15,11 @@ spring.datasource.url=jdbc:postgresql://localhost/<DB-NAME>
 spring.datasource.username=<DB-USER>
 spring.datasource.password=<DB-PASSWORD>
 
+spring.jpa.hibernate.ddl-auto=update
+spring.jpa.show-sql=true
+spring.jpa.properties.hibernate.format_sql=true
+spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.PostgreSQL81Dialect
+
 spring.data.rest.base-path=/api
 
 management.endpoints.web.exposure.include=*
--- src/main/java/gr/hua/dit/ds/reference/letter/service/config/WebSecurityConfig.java
+++ src/main/java/gr/hua/dit/ds/reference/letter/service/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,12 +21,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder())
-                .usersByUsernameQuery("select username,password, enabled from user where username=?")
+                .usersByUsernameQuery("select username,password, enabled from users where username=?")
                 .authoritiesByUsernameQuery("select username, authority from authorities where username=?");
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {}
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and().authorizeRequests()
+                .antMatchers("/**").hasRole("ADMIN")
+                .and().csrf().disable().headers().frameOptions().disable()
+                .and().formLogin().permitAll().and().logout().permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+        web.ignoring().antMatchers("/");
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {

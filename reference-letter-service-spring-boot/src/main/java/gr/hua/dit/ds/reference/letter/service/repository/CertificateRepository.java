93007a10fb4f9ee06d3595ab9010d8e76a9451a8
it21846 <61410055+it21846@users.noreply.github.com>
2022-02-18 18:38:38 +0200

    Delete reference-letter-service-spring-boot directory

--- reference-letter-service-spring-boot/src/main/java/gr/hua/dit/ds/reference/letter/service/repository/CertificateRepository.java
+++
@@ -1,9 +0,0 @@
-package gr.hua.dit.ds.reference.letter.service.repository;
-
-import gr.hua.dit.ds.reference.letter.service.entity.Certificate;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@RepositoryRestResource(path="certificates")
-public interface CertificateRepository extends JpaRepository<Certificate, Integer>{
-}
--- reference-letter-service-spring-boot/src/main/java/gr/hua/dit/ds/reference/letter/service/repository/ReferenceLetterRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package gr.hua.dit.ds.reference.letter.service.repository;
-
-import gr.hua.dit.ds.reference.letter.service.entity.ReferenceLetterRequest;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(path="requests")
-public interface ReferenceLetterRequestRepository extends CrudRepository<ReferenceLetterRequest, Integer> {
-}

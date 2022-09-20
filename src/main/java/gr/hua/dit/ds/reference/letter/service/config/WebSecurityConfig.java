c766a1b4bd51e1231b48fcaa5cc718c19ef975a6
panagiotisbellias <belliaspan@gmail.com>
2022-09-20 14:42:22 +0300

    origin correction

--- src/main/java/gr/hua/dit/ds/reference/letter/service/config/WebSecurityConfig.java
+++ src/main/java/gr/hua/dit/ds/reference/letter/service/config/WebSecurityConfig.java
@@ -14,8 +14,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 @Configuration
 @EnableWebSecurity
@@ -99,7 +98,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         // Don't do this in production, use a proper list  of allowed origins
-        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+        //config.setAllowedOriginPatterns(Collections.singletonList("*"));
+        List<String> origins = new ArrayList<String>();
+        origins.add("http://localhost:8080/");
+        config.setAllowedOriginPatterns(origins);
         config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
         source.registerCorsConfiguration("/**", config);
